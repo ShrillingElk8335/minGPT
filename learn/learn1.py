5958766af1aa38636a12522f6623f91b83348cb4
ShrillingElk8335 <ShrillingElk8335@ioiio.cc>
2026-05-22 18:38:20 +0700

    update

--- learn/learn1.py
+++ learn/learn1.py
@@ -39,3 +39,31 @@ for t in range(block_size):
     target = y[t]
     print(f"when input is {context} the target is {target}")
 
+torch.manual_seed(42)
+batch_size = 4 # how many independent sequences will we process in parallel?
+block_size = 8 # what is the maximum context length for predictions?
+
+def get_batch(split):
+    data = train_data if split == "train" else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    print("ix:", ix)
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+xb, yb = get_batch("train")
+print("inputs:")
+print(xb.shape)
+print(xb)
+print("targets:")
+print(yb.shape)
+print(yb)
+
+print("----")
+
+for b in range(batch_size):
+    for t in range(block_size):
+        context = xb[b, :t+1]
+        target = yb[b, t]
+        print(f"when input is {context} the target is {target}")
+
